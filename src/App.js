a48dd363e706279be52536b458a9db4e261cb419
ManojKChowdhury <manojc009@gmail.com>
2020-01-22 01:19:32 +0100

    Added multiple components and pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from "./pages/signing/signing.component";
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInAndSignUp}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
